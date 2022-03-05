fb92b620428e27e364e6bdae79260524fd083af8
IsmailIbrahim84 <beisso75@hotmail.com>
2022-03-05 17:21:32 +0200

    Adding and Creating Projects

---
+++ SignalRDemo/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace SignalRDemo
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+        }
+    }
+}
