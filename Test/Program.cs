f821bc74aac44ad20e8566a1af0400a72c5cd793
IsmailIbrahim84 <beisso75@hotmail.com>
2022-03-12 20:44:23 +0200

    Latest

--- Test/Program.cs
+++ Test/Program.cs
@@ -22,36 +22,58 @@
 //Console.WriteLine(solution(2004));;
 
 
-bool solution(string inputString)
-{
-    var watch1 = new System.Diagnostics.Stopwatch();
-    var watch2 = new System.Diagnostics.Stopwatch();
-    string reversed = "";        
-    watch1.Start();
+//bool solution(string inputString)
+//{
+//    var watch1 = new System.Diagnostics.Stopwatch();
+//    var watch2 = new System.Diagnostics.Stopwatch();
+//    string reversed = "";        
+//    watch1.Start();
 
-    //More faster in the huge amount of string:
-    char[] charArray = inputString.ToCharArray();
-    Array.Reverse( charArray );
-    reversed = new string(charArray);
-    //----------------------------
-    watch1.Stop();
+//    //More faster in the huge amount of string:
+//    char[] charArray = inputString.ToCharArray();
+//    Array.Reverse( charArray );
+//    reversed = new string(charArray);
+//    //----------------------------
+//    watch1.Stop();
 
-    //Slower
-    reversed = "";
-    watch2.Start();
+//    //Slower
+//    reversed = "";
+//    watch2.Start();
     
-    char[] temp = inputString.ToCharArray();
+//    char[] temp = inputString.ToCharArray();
     
-    for (int i = temp.Length - 1; i >= 0; i--)
-    {
-        reversed += temp[i].ToString();
-    }
-    watch2.Stop();
-    Console.WriteLine($"Execution Time-Watch1: {watch1.Elapsed} ElapsedTime, Execution Time-Watch2: {watch2.Elapsed} ElapsedTime");
-   // if (watch1.)
-    //return inputString == new string(charArray);
-    return inputString == reversed;
+//    for (int i = temp.Length - 1; i >= 0; i--)
+//    {
+//        reversed += temp[i].ToString();
+//    }
+//    watch2.Stop();
+//    Console.WriteLine($"Execution Time-Watch1: {watch1.Elapsed} ElapsedTime, Execution Time-Watch2: {watch2.Elapsed} ElapsedTime");
+//   // if (watch1.)
+//    //return inputString == new string(charArray);
+//    return inputString == reversed;
 
-}
+//}
 
-Console.WriteLine(solution("saippuakivikauppias"));
+//Console.WriteLine(solution("saippuakivikauppias"));
+
+
+
+//////////////////////////////////////////////////////
+
+//int[] inputArray = {-23, 4, -3, 8, -12};
+
+//int Solution(int[] inputArray){
+//    int result= Int32.MinValue;
+//    for (int i = 0; i < inputArray.Length-1; i++)
+//    {
+        
+//        if (inputArray[i] * inputArray[i + 1] > result)
+//            result = inputArray[i] * inputArray[i + 1];
+        
+        
+//    }
+    
+//    return result;
+//}
+//Console.WriteLine(Solution(inputArray));
+///////////////////////////////////////////////////////
